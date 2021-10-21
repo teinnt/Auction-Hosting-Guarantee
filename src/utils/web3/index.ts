8bc26b6252d2a7948fb85d3932fc97a372441ed8
teinnt <phuongthao234567@gmail.com>
2021-10-22 08:15:24 +1300

    Update web3 provider

--- src/utils/web3/index.ts
+++ src/utils/web3/index.ts
@@ -1,7 +1,14 @@
 import Web3 from 'web3'
+import HDWalletProvider from '@truffle/hdwallet-provider'
+
 import ItemFactory from '../../ethereum/abis/ItemFactory.json'
 
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.REACT_APP_RINKEBY_URL || ''))
+const web3 = new Web3(
+  new HDWalletProvider({
+    mnemonic: process.env.REACT_APP_MNEMONIC || '',
+    providerOrUrl: process.env.REACT_APP_RINKEBY_URL || '',
+  })
+)
 
 const getAccount = async () => (await web3.eth.getAccounts())[0]
 
